6f85f38fdd1bc1e03902fc909a53828902393534
JuanT05 <jtsebastiantarazona@gmail.com>
2025-10-29 23:47:48 -0500

    Configura conexión y sesión de base de datos SQLite

--- database.py
+++ database.py
@@ -1,8 +1,20 @@
-from sqlmodel import SQLModel, create_engine
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./biblioteca.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./biblioteca.db"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def init_db():
-    SQLModel.metadata.create_all(engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
