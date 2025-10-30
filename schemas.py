f8ea6d7f1ede5774bf692f730b9bb8d4e428aa1a
JuanT05 <jtsebastiantarazona@gmail.com>
2025-10-29 23:52:05 -0500

    Crea esquemas Pydantic para validación de datos

---
+++ schemas.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class LibroBase(BaseModel):
+    titulo: str
+    isbn: str
+    anio_publicacion: Optional[int]
+    copias_disponibles: Optional[int] = 1
+
+
+class LibroCreate(LibroBase):
+    autor_id: int
+
+
+class Libro(LibroBase):
+    id: int
+    autor_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class AutorBase(BaseModel):
+    nombre: str
+    pais_origen: Optional[str]
+    anio_nacimiento: Optional[int]
+
+
+class AutorCreate(AutorBase):
+    pass
+
+
+class Autor(AutorBase):
+    id: int
+    libros: List[Libro] = []
+
+    class Config:
+        orm_mode = True
